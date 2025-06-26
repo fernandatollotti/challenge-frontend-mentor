7860aea33f11ff6d129162d8fc2fde27424eb5ae
Fernanda Tollotti <71778388+fernandatollotti@users.noreply.github.com>
2025-06-25 21:22:41 -0300

    feat: add footer

--- cards-feature/src/App.tsx
+++ cards-feature/src/App.tsx
@@ -4,23 +4,37 @@ import { services } from "./data/services/services";
 
 function App() {
   return (
-    <section className={`${styles.services} ${styles.container}`}>
-      <div className={styles.intro}>
-        <h2>
-          <span className={styles.lightText}>Reliable, efficient delivery</span>
-          Powered by Technology
-        </h2>
-        <p>
-          Our Artificial Intelligence powered tools use millions of project data
-          points to ensure that your project is successful
+    <>
+      <section className={`${styles.services} ${styles.container}`}>
+        <div className={styles.intro}>
+          <h2>
+            <span className={styles.lightText}>
+              Reliable, efficient delivery
+            </span>
+            Powered by Technology
+          </h2>
+          <p>
+            Our Artificial Intelligence powered tools use millions of project
+            data points to ensure that your project is successful
+          </p>
+        </div>
+        <div className={styles.cardContainer}>
+          {services.map((service) => (
+            <Card key={service.title} {...service} />
+          ))}
+        </div>
+      </section>
+      <footer className={styles.footer}>
+        <p className={styles.attribution}>
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a href="https://fernandatollotti.com.br">Fernanda Tollotti</a>.
         </p>
-      </div>
-      <div className={styles.cardContainer}>
-        {services.map((service) => (
-          <Card key={service.title} {...service} />
-        ))}
-      </div>
-    </section>
+      </footer>
+    </>
   );
 }
 
